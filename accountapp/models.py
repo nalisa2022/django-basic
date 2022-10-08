edb115dddc6a3066ef2ed6a1e210abb7017cd437
nalisa <nalisa2022.lao@gmail.com>
2022-10-08 14:52:23 +0700

    model migration try helloworld model

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class helloworld(models.Model):
+    text=models.CharField(max_length=255, null=False)
