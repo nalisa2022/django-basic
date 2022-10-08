f0f3923fd5cd86bf0e959d913c6237765d17fe02
nalisa <nalisa2022.lao@gmail.com>
2022-10-08 15:58:48 +0700

    post

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import hello_world, home
+from .views import home, test
 
 app_name='accountapp' # localhost:8000/account/helloworld = accountapp:hello_world
 
 urlpatterns = [
-	# path('/helloworld', hello_world, name='hello_world'),
 	path('', home ),
+	path('test', test),
 ]
 
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+# from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def hello_world(request):
-    return HttpResponse('Hello world')
 
+# @csrf_exempt
 def home(request):
     return render(request, "accountapp/hello.html")
+
+def test(request):
+    # return HttpResponse('Hello')
+
+    if request.method=='POST':
+        return render(request, "accountapp/hello.html", context={'text':'POST method !'})
+    else:
+        return render(request, "accountapp/hello.html", context={'text':'Get method !'})
