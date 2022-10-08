9a80496f95153de71932b4567f5a2505914a2818
nalisa <nalisa2022.lao@gmail.com>
2022-10-08 16:29:37 +0700

    Get posting value, modeling, save to DB

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
+from hashlib import new
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import helloworld
 # from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -12,6 +14,13 @@ def test(request):
     # return HttpResponse('Hello')
 
     if request.method=='POST':
-        return render(request, "accountapp/hello.html", context={'text':'POST method !'})
+        temp=request.POST.get('text')
+
+        # modeling
+        new_data=helloworld()
+        new_data.text=temp
+        new_data.save()
+
+        return render(request, "accountapp/hello.html", context={'new_data_output': new_data})
     else:
         return render(request, "accountapp/hello.html", context={'text':'Get method !'})
